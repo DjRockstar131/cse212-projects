4acb6f976d1207ad987e8ee2fd5d7b46760aae45
Davin Quist <davinjquist@gmail.com>
2026-01-15 19:02:56 -0700

    main

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -2,21 +2,14 @@
     public static void Run() {
         // Test Cases
 
-        // Test 1
-        // Scenario: Enqueue one value and then Dequeue it.
-        // Expected Result: It should display 100
         Console.WriteLine("Test 1");
         var queue = new SimpleQueue();
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
 
         Console.WriteLine("------------");
 
-        // Test 2
-        // Scenario: Enqueue multiple values and then Dequeue all of them
-        // Expected Result: It should display 200, then 300, then 400 in that order
         Console.WriteLine("Test 2");
         queue = new SimpleQueue();
         queue.Enqueue(200);
@@ -28,13 +21,9 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
 
         Console.WriteLine("------------");
 
-        // Test 3
-        // Scenario: Dequeue from an empty Queue
-        // Expected Result: An exception should be raised
         Console.WriteLine("Test 3");
         queue = new SimpleQueue();
         try {
@@ -44,30 +33,24 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
     }
 
     private readonly List<int> _queue = new();
 
-    /// <summary>
-    /// Enqueue the value provided into the queue
-    /// </summary>
-    /// <param name="value">Integer value to add to the queue</param>
-    private void Enqueue(int value) {
-        _queue.Insert(0, value);
+    // Must be public so tests can call it
+    public void Enqueue(int value) {
+        // add to the back
+        _queue.Add(value);
     }
 
-    /// <summary>
-    /// Dequeue the next value and return it
-    /// </summary>
-    /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
-    /// <returns>First integer in the queue</returns>
-    private int Dequeue() {
+    // Must be public so tests can call it
+    public int Dequeue() {
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        // remove from the front
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
